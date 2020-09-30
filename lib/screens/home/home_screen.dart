d93e2d532e8c15ba2094f8bd71ab5cbb8d99b326
cvargas22 <cesar-ed22@hotmail.com>
2020-09-30 11:18:31 -0300

    se crea y muestra card de pokemon + bonus animacion

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex/models/pokemon_list_model.dart';
+import 'package:pokedex/screens/home/components/pokemon_card.dart';
 import 'package:pokedex/services/api_provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin{
   final ApiProvider apiProvider = ApiProvider();
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 4000)
+    );
+    controller.repeat();
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
+          Positioned(
+            right: -20,
+            top: -20,
+            child: RotationTransition(
+              turns: Tween(begin: 0.0, end: 1.0).animate(controller),
+              child: Image.asset(
+                'assets/images/pokeball.png',
+                height: 250,
+                color: Color(0xffe3e3e3).withAlpha(100),
+              ),
+            ),
+          ),
           Container(
             padding: EdgeInsets.symmetric(horizontal: 25),
             child: CustomScrollView(
@@ -32,16 +63,12 @@ class HomeScreen extends StatelessWidget {
                   future: apiProvider.obtenerListaPokemon(),
                   builder: (BuildContext context, AsyncSnapshot<List<PokemonListModel>> snapshot){
                     if(snapshot.hasData){
-                      print(snapshot.data[10].name);
                       return SliverGrid.count(
-                        crossAxisCount: 1,
-                        crossAxisSpacing: 0,
-                        mainAxisSpacing: 0,
-                        children: [
-                          Center(
-                            child: Text('${snapshot.data[10].name}'),
-                          )
-                        ],
+                        childAspectRatio: 1.4,
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
+                        children: snapshot.data.map((poke) => PokemonCard(pokemon: poke)).toList(),
                       );
                     }
                     else if(snapshot.hasError){
